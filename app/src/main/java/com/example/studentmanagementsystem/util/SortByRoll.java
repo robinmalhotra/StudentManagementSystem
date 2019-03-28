673d90290d1055835a56c334dcbc97c5b5bec91d
robinmalhotra <robinmalhotra@mail.com>
2019-03-28 16:21:12 +0530

    changes in roll sorting

--- app/src/main/java/com/example/studentmanagementsystem/util/SortByRoll.java
+++ app/src/main/java/com/example/studentmanagementsystem/util/SortByRoll.java
@@ -8,6 +8,10 @@ public class SortByRoll implements Comparator<StudentTemplate> {
 
     @Override
     public int compare(StudentTemplate o1, StudentTemplate o2) {
-        return (o1.getStudentTemplateRoll().compareTo(o2.getStudentTemplateRoll()));
+        if(Integer.parseInt(o1.getStudentTemplateRoll())>Integer.parseInt(o2.getStudentTemplateRoll()))
+            return 1;
+        else
+            return -1;
+//        return (o1.getStudentTemplateRoll().compareTo(o2.getStudentTemplateRoll()));
     }
 }
